218de299e5b3fd69275fd5e1c4a22cb9d02915b9
naminh <minh.nguyenanh@axonactive.vn>
2016-03-02 10:25:30 +0700

    setup project

---
+++ Objective-C/Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objective-C
+//
+//  Created by Nguyen Anh Minh on 3/2/16.
+//  Copyright © 2016 Minh Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C/Objective-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objective-C
+//
+//  Created by Nguyen Anh Minh on 3/2/16.
+//  Copyright © 2016 Minh Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
